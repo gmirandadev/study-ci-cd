4f5eee724d54181cf4e72a1c3e47fba9362a3eb3
Guilherme Miranda <guilhermealvesmiranda99@gmail.com>
2021-05-15 10:18:32 -0300

    fix: change port to aws elastic bean talk

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 // App
 const app = express();
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     return res.json({message: 'server is up'});
 });
 
-app.listen(3333);
+app.listen(port);
